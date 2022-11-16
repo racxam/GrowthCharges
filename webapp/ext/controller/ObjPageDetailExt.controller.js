78dc676c8ea64050243d20fe7093899fc82ddf34
Dipankar Mohanty <dmohanty@convergentis.com>
2022-11-16 12:40:28 +0530

    HeaderFacet

---
+++ webapp/ext/controller/ObjPageDetailExt.controller.js
@@ -0,0 +1,134 @@
+sap.ui.define([
+    "sap/m/Dialog",
+    "sap/m/TextArea",
+    "sap/m/Button"
+],
+function (Dialog,TextArea,Button){
+    "use strict";
+    return sap.ui.controller("com.gc.dashboard.ext.controller.ObjPageDetailExt", {
+
+        showStatusCIL : function(sStatus){
+            if(sStatus === "INP"){
+                return "Information";
+            } else if(sStatus === "PCILAPP"){
+                return "Warning";
+            }else if(sStatus === "PCILAPP"){
+                return "Error";
+            }else if(sStatus === "CILA"){
+                return "Success";
+            }else if(sStatus === "PFIAPP"){
+                return "Success";
+            }else if(sStatus === "CLSD"){
+                return "Information";
+            }else if(sStatus === "HLD"){
+                return "Information";
+            }else if(sStatus === "FRJ"){
+                return "Error";
+            }else if(sStatus === "PCLSD"){
+                return "Information";
+            }else{
+                return "None";
+            }
+        }, 
+        showStatusCBC : function(sStatus){
+            if(sStatus === "INP"){
+                return "Information";
+            } else if(sStatus === "PCILAPP"){
+                return "Warning";
+            }else if(sStatus === "PCILAPP"){
+                return "Error";
+            }else if(sStatus === "CILA"){
+                return "Success";
+            }else if(sStatus === "PFIAPP"){
+                return "Success";
+            }else if(sStatus === "CLSD"){
+                return "Information";
+            }else if(sStatus === "HLD"){
+                return "Information";
+            }else if(sStatus === "FRJ"){
+                return "Error";
+            }else if(sStatus === "PCLSD"){
+                return "Information";
+            }else{
+                return "None";
+            }
+        }, 
+        showStatusDC : function(sStatus){
+            if(sStatus === "INP"){
+                return "Information";
+            } else if(sStatus === "PCILAPP"){
+                return "Warning";
+            }else if(sStatus === "PCILAPP"){
+                return "Error";
+            }else if(sStatus === "CILA"){
+                return "Success";
+            }else if(sStatus === "PFIAPP"){
+                return "Success";
+            }else if(sStatus === "CLSD"){
+                return "Information";
+            }else if(sStatus === "HLD"){
+                return "Information";
+            }else if(sStatus === "FRJ"){
+                return "Error";
+            }else if(sStatus === "PCLSD"){
+                return "Information";
+            }else{
+                return "None";
+            }
+        },
+        
+        showStatusRefund : function(sStatus){
+            if(sStatus === "INP"){
+                return "Information";
+            } else if(sStatus === "PCILAPP"){
+                return "Warning";
+            }else if(sStatus === "PCILAPP"){
+                return "Error";
+            }else if(sStatus === "CILA"){
+                return "Success";
+            }else if(sStatus === "PFIAPP"){
+                return "Success";
+            }else if(sStatus === "CLSD"){
+                return "Information";
+            }else if(sStatus === "HLD"){
+                return "Information";
+            }else if(sStatus === "FRJ"){
+                return "Error";
+            }else if(sStatus === "PCLSD"){
+                return "Information";
+            }else{
+                return "None";
+            }
+        },
+        showStatusPayment : function(sStatus){
+            if(sStatus === "INP"){
+                return "Information";
+            } else if(sStatus === "PCILAPP"){
+                return "Warning";
+            }else if(sStatus === "PCILAPP"){
+                return "Error";
+            }else if(sStatus === "CILA"){
+                return "Success";
+            }else if(sStatus === "PFIAPP"){
+                return "Success";
+            }else if(sStatus === "CLSD"){
+                return "Information";
+            }else if(sStatus === "HLD"){
+                return "Information";
+            }else if(sStatus === "FRJ"){
+                return "Error";
+            }else if(sStatus === "PCLSD"){
+                return "Information";
+            }else{
+                return "None";
+            }
+        },
+
+        setIcon : function(){
+            sap.ui.core.IconPool.addIcon("CIL", "customfont", "icomoon", "e900");
+            return "./img/CIL.png";
+        }
+    
+    });
+    });
+    
