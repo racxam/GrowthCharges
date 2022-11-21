09db45a54ac1f5098bba90b4d077f1e9e1363202
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2022-11-21 13:02:11 +0530

    minor changes

--- webapp/ext/controller/DetailsExt.controller.js
+++ webapp/ext/controller/DetailsExt.controller.js
@@ -6,7 +6,9 @@ sap.ui.define([
 function (Dialog,TextArea,Button){
     "use strict";
     return sap.ui.controller("com.gc.dashboard.ext.controller.DetailsExt", {
-
+        onInit: function() {
+            var oTable = this.getView().getContent()[0].getAggregation("sections")[2].getSubSections()[1].getBlocks()[0].getContent()[0];
+        },
         showStatusCIL1 : function(sStatus){
             if(sStatus === "INP"){
                 return "Information";
