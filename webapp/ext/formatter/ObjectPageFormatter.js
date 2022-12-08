55950ee39ba5ff58e053ce094ab7154e396d2d22
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2022-12-08 12:54:55 +0530

    attachments

--- webapp/ext/formatter/ObjectPageFormatter.js
+++ webapp/ext/formatter/ObjectPageFormatter.js
@@ -1,36 +1,36 @@
 jQuery.sap.declare("com.gc.dashboard.ext.formatter.ObjectPageFormatter");
 com.gc.dashboard.ext.formatter.ObjectPageFormatter = {
-    getCBCObjectKey: function(requestID, version, DraftGuid){
+    getCBCObjectKey: function(requestID, version1, DraftGuid){
         var sObjectKey = "";
         if(requestID){
-            sObjectKey = `${requestID}${version}CBC`;
+            sObjectKey = `${requestID}${version1}CBC`;
         }else{
             sObjectKey = DraftGuid;
         }            
         return sObjectKey;
     },
-    getDCObjectKey: function(requestID, version, DraftGuid){
+    getDCObjectKey: function(requestID, version1, DraftGuid){
         var sObjectKey = "";
         if(requestID){
-            sObjectKey = `${requestID}${version}DC`;
+            sObjectKey = `${requestID}${version1}DC`;
         }else{
             sObjectKey = DraftGuid;
         }            
         return sObjectKey;
     },
-    getCILObjectKey: function(requestID, version, DraftGuid){
+    getCILObjectKey: function(requestID, version1, DraftGuid){
         var sObjectKey = "";
         if(requestID){
-            sObjectKey = `${requestID}${version}CIL`;
+            sObjectKey = `${requestID}${version1}CIL`;
         }else{
             sObjectKey = DraftGuid;
         }            
         return sObjectKey;
     },
-    getPayObjectKey: function(requestID, version, DraftGuid){
+    getPayObjectKey: function(requestID, version1, DraftGuid){
         var sObjectKey = "";
         if(requestID){
-            sObjectKey = `${requestID}${version}PAY`;
+            sObjectKey = `${requestID}${version1}PAY`;
         }else{
             sObjectKey = DraftGuid;
         }            
