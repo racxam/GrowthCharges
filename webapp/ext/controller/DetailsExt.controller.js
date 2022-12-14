23c98c44e0eb5a4bd45c076bbcbd438828fdc190
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2022-12-14 14:17:58 +0530

    Enable icontabbar for 1.71

--- webapp/ext/controller/DetailsExt.controller.js
+++ webapp/ext/controller/DetailsExt.controller.js
@@ -15,7 +15,9 @@ function (){
                 //If opened not using FLP, there will be an error              
             }
 
-            
+            //Enable icontabbar mode
+            const oIconTabBar = sap.ui.getCore().byId("com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--objectPage");
+            oIconTabBar.setUseIconTabBar(true);            
         },
         onAfterRendering: function() {
 
