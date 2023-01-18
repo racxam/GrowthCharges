e4f16f78c58680c4ff71160fea64ae2f2d2d7845
Dipankar Mohanty <dmohanty@convergentis.com>
2023-01-18 18:54:32 +0530

    Side Effects for DC and Submit

--- webapp/ext/controller/DetailsExt.controller.js
+++ webapp/ext/controller/DetailsExt.controller.js
@@ -105,7 +105,7 @@ sap.ui.define(
         const setBlocksRight = function () {
           var blocks = this.getBlocks();
           for (var i = 0; i < blocks.length; i++) {
-            if(blocks[i].getParent().getParent().getId() === "com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--CBCHeader-GI::SubSection"){
+            if(blocks[i].getParent().getParent().getId() === "com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--CBCHeader-GI::SubSection" || blocks[i].getParent().getParent().getId() === "com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--DCHeader-GI::SubSection"){
               blocks[i].getContent()[0].getLayout().setColumnsM(1);
               blocks[i].getContent()[0].getLayout().setColumnsL(1);
               blocks[i].getContent()[0].getLayout().setColumnsXL(1);
@@ -118,8 +118,6 @@ sap.ui.define(
         };
         const cbcGenSubSection = sap.ui.getCore().byId("com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--CBCHeader-GI::SubSection");
         if (cbcGenSubSection) {
-          
-          
           cbcGenSubSection.onAfterRendering = setBlocksRight;
         }
 
@@ -179,6 +177,11 @@ sap.ui.define(
         }
 
         //Set the Grid Layout for the SubSection
+        const dcGenSubSection = sap.ui.getCore().byId("com.gc.dashboard::sap.suite.ui.generic.template.ObjectPage.view.Details::zgc_c_requests--DCHeader-GI::SubSection");
+        if (dcGenSubSection) {
+          dcGenSubSection.onAfterRendering = setBlocksRight;
+        }
+
         const dcExemptionSection = sap.ui
           .getCore()
           .byId(
