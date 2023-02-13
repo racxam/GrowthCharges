7a2a5594fbd7cebc1732a124894871fa5ff8519c
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2023-02-13 09:57:15 +0530

    Basic testing using wdi5

--- webapp/test/homepage/basic.test.js
+++ webapp/test/homepage/basic.test.js
@@ -1,9 +1,6 @@
 const { wdi5 } = require("wdio-ui5-service")
 
 describe("Load the app", () => {
-//   before(async () => {
-//     await wdi5.goTo("#comgcdashboard-tile")
-//   })
 
   it("SmartFilterbar has loaded", async () => {
     const filterBar = {
@@ -14,5 +11,32 @@ describe("Load the app", () => {
     }
     const prop = await browser.asControl(filterBar).getProperty("visible");
     expect(prop).toEqual(true);
-  })
-})
+  });
+
+  //Clicking on the Go button should load atleast one entry in the table
+  it("SmartTable has loaded with atleast one row", async () => {
+    // Go button selector
+    const goButton = browser.asControl({
+        selector: {
+            id: "listReportFilter-btnGo",
+            viewName: "com.gc.dashboard::sap.suite.ui.generic.template.ListReport.view.ListReport::zgc_c_requests",
+            interactable: true
+        }
+    });
+    // Click on the Go button
+    console.log(goButton);
+    await goButton.firePress();
+
+    // SmartTable selector
+    const smartTable = {
+        selector: {
+            controlType: "sap.ui.comp.smarttable.SmartTable",
+            interactable: true
+        }
+    }
+    // Get the number of rows in the table
+    const rowCount = await browser.asControl(smartTable).getProperty("rowCount");
+    // Check if the table has atleast one row
+    expect(rowCount).toBeGreaterThan(0);
+  });
+});
