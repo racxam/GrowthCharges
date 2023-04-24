4474ece520ff7403398f88054033cc5aa181cd2b
Dipankar Mohanty <dmohanty@convergentis.com>
2023-04-24 16:41:13 +0530

    Change of Sort Date PaymentsTable

--- webapp/ext/controller/DetailsExt.controller.js
+++ webapp/ext/controller/DetailsExt.controller.js
@@ -613,8 +613,8 @@ sap.ui.define(
           // Add property 'Gen_pay_receipt_ac' to $select
           oEvent.getParameter("bindingParams").parameters.select = oEvent.getParameter("bindingParams").parameters.select + ",Gen_pay_receipt_ac,deferral_adjust";
           oEvent.getParameter("bindingParams").sorter = [
-            new sap.ui.model.Sorter("payment_date", false),
-            new sap.ui.model.Sorter("document_no", false)
+            new sap.ui.model.Sorter("sort_date", false),
+            new sap.ui.model.Sorter("sort_document", false)
           ];
         }
         if (oEvent.getSource().getId() ===
